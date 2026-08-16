7c065f0234c4e13950d69a74c5d713233301b5ab
Ashish Patel <ashishpatel3630@gmail.com>
2026-08-16 11:52:54 +0530

    folder dashboard creating

--- Design_os/src/pages/Dashboard.jsx
+++ Design_os/src/pages/Dashboard.jsx
@@ -1,35 +1,36 @@
 import React from "react";
 
-import DashboardSidebar from "../components/dashboard/DashboardSidebar";
-import DashboardTopbar from "../components/dashboard/DashboardTopbar";
-import DashboardHeader from "../components/dashboard/DashboardHeader";
-import QuickActions from "../components/dashboard/QuickActions";
-import RecentProjects from "../components/dashboard/RecentProjects";
-import Activity from "../components/dashboard/Activity";
+// import DashboardSidebar from "../components/dashboard/DashboardSidebar";
+// import DashboardTopbar from "../components/dashboard/DashboardTopbar";
+// import DashboardHeader from "../components/dashboard/DashboardHeader";
+// import QuickActions from "../components/dashboard/QuickActions";
+// import RecentProjects from "../components/dashboard/RecentProjects";
+// import Activity from "../components/dashboard/Activity";
 
 function Dashboard() {
   return (
-    <div className="min-h-screen bg-[#F7F7F7] text-[#111111]">
-      {/* Sidebar */}
-      <DashboardSidebar />
+    <></>
+    // <div className="min-h-screen bg-[#F7F7F7] text-[#111111]">
+    //   {/* Sidebar */}
+    //   <DashboardSidebar />
 
-      {/* Main content */}
-      <div className="ml-[260px] min-h-screen">
-        {/* Top navigation */}
-        <DashboardTopbar />
+    //   {/* Main content */}
+    //   <div className="ml-[260px] min-h-screen">
+    //     {/* Top navigation */}
+    //     <DashboardTopbar />
 
-        {/* Dashboard */}
-        <main className="p-6 lg:p-8">
-          <DashboardHeader />
+    //     {/* Dashboard */}
+    //     <main className="p-6 lg:p-8">
+    //       <DashboardHeader />
 
-          <QuickActions />
+    //       <QuickActions />
 
-          <RecentProjects />
+    //       <RecentProjects />
 
-          <Activity />
-        </main>
-      </div>
-    </div>
+    //       <Activity />
+    //     </main>
+    //   </div>
+    // </div>
   );
 }
 
